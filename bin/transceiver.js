484e44e87bd89d88030a6d5c3bcd607b9975e6db
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2022-02-06 13:09:02 -0800

    - request, yargs; + commander

--- bin/transceiver.js
+++ bin/transceiver.js
@@ -3,96 +3,98 @@
 
 const http = require('http');
 const chp = require('child_process');
+const process = require('process');
+
+const ip = require('ip');
 const fs = require('fs-extra');
 const express = require('express');
 const bodyParser = require('body-parser');
-const ip = require('ip');
 const portfinder = require('portfinder');
-const rp = require('request-promise-native');
 const json5 = require('json5');
-const yargs = require('yargs');
+// const yargs = require('yargs');
+
+const { program } = require('commander');
+
+const rpg = require('../lib/rpg.js');
 
 const msleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
 
-const argv = yargs
-  .option('ip', {
-    describe: 'SDRangel IP address',
-    default: '127.0.0.1:8091'
-  })
-  .option('config', {
-    alias: 'c',
-    describe: 'SDR config file'
-  })
-  .version()
-  .help()
-  .argv;
+const rx = ['deviceset', 0];
+const tx = ['deviceset', 1];
 
-const baseUri = `http://${argv.ip}/sdrangel`;
-
-function angel () {
-  let p = [baseUri];
-  for (let i = 0; i < arguments.length; i++) {
-    p.push(arguments[i]);
-  }
-  return function () {
-    let p1 = [];
-    for (let i = 0; i < arguments.length; i++) {
-      p1.push(arguments[i]);
-    }
-    return p.concat(p1).join('/');
-  };
-}
-
-const rx = angel('deviceset', 0);
-const tx = angel('deviceset', 1);
-
-const initConfig = async () => {
+const initConfig = async (rp, opts) => {
   // client
-  const sdrangel = chp.spawn('sdrangel');
-  sdrangel.stdout.on('data', (data) => { console.log(`stdout: ${data}`); });
-  sdrangel.stderr.on('data', (data) => { console.log(`stderr: ${data}`); });
-  sdrangel.on('close', (code) => { console.log(`child process exited with code ${code}`); });
+  const sdrangel = chp.spawn('sdrangel', {stdio: 'inherit'});
+
+  const exiter = (code) => {
+    console.log('sdrangel exited with code:', code);
+    process.exit(code);
+  };
+  sdrangel.on('close', exiter);
+  sdrangel.on('exit', exiter);
+
   await msleep(5000);
 
-  await rp({method: 'PUT', uri: rx('device'), json: true, body: {hwType: 'PlutoSDR', direction: 0}});
-  await rp({method: 'POST', uri: rx('channel'), json: true, body: {channelType: 'SSBDemod', direction: 0}});
+  await rp({method: 'PUT',  path: [...rx, 'device'],  body: {hwType: 'PlutoSDR', direction: 0}});
+  await rp({method: 'POST', path: [...rx, 'channel'], body: {channelType: 'SSBDemod', direction: 0}});
 
-  await rp({method: 'POST', uri: baseUri + '/deviceset?direction=1'});
-  await rp({method: 'PUT', uri: tx('device'), json: true, body: {hwType: 'PlutoSDR', direction: 1}});
-  await rp({method: 'POST', uri: tx('channel'), json: true, body: {channelType: 'SSBMod', direction: 1}});
+  await rp({method: 'POST', path: ['deviceset?direction=1']});
+  await rp({method: 'PUT',  path: [...tx, 'device'],  body: {hwType: 'PlutoSDR', direction: 1}});
+  await rp({method: 'POST', path: [...tx, 'channel'], body: {channelType: 'SSBMod', direction: 1}});
 
-  if (argv.config) {
-    const fbody = await fs.readFile(argv.config, 'utf8');
+  if (opts.config) {
+    const fbody = await fs.readFile(opts.config, 'utf8');
     const config = json5.parse(fbody);
 
-    let rxset = await rp({uri: rx('device', 'settings'), json: true});
+    let rxset = await rp({path: [...rx, 'device', 'settings']});
     console.log(rxset);
     Object.assign(rxset, config[0].device);
-    await rp({method: 'PATCH', uri: rx('device', 'settings'), json: true, body: rxset});
+    await rp({method: 'PATCH', path: [...rx, 'device', 'settings'], body: rxset});
 
-    let demodset = await rp({uri: rx('channel', 0, 'settings'), json: true});
+    let demodset = await rp({path: [...rx, 'channel', 0, 'settings']});
     console.log(demodset);
     Object.assign(demodset, config[0].channel);
-    await rp({method: 'PATCH', uri: rx('channel', 0, 'settings'), json: true, body: demodset});
+    await rp({method: 'PATCH', path: [...rx, 'channel', 0, 'settings'], body: demodset});
 
-    let txset = await rp({uri: tx('device', 'settings'), json: true});
+    let txset = await rp({path: [...tx, 'device', 'settings']});
     console.log(txset);
     Object.assign(txset, config[1].device);
-    await rp({method: 'PATCH', uri: tx('device', 'settings'), json: true, body: txset});
+    await rp({method: 'PATCH', path: [...tx, 'device', 'settings'], body: txset});
 
-    let modset = await rp({uri: tx('channel', 0, 'settings'), json: true});
+    let modset = await rp({path: [...tx, 'channel', 0, 'settings']});
     console.log(modset);
     Object.assign(modset, config[1].channel);
-    await rp({method: 'PATCH', uri: tx('channel', 0, 'settings'), json: true, body: modset});
-
+    await rp({method: 'PATCH', path: [...tx, 'channel', 0, 'settings'], body: modset});
   }
 
-  await rp({method: 'POST',  uri: rx('device', 'run'), json: true});
-  await rp({method: 'PATCH', uri: rx('focus'), json: true});
-  await rp({method: 'POST',  uri: tx('device', 'run'), json: true});
+  await rp({method: 'POST',  path: [...rx, 'device', 'run']});
+  await rp({method: 'PATCH', path: [...rx, 'focus']});
+  await rp({method: 'POST',  path: [...tx, 'device', 'run']});
 };
 
 const main = async () => {
+  // const argv = yargs
+  // .option('ip',     {alias: 'i', describe: 'SDRangel IP address', default: '127.0.0.1:8091'})
+  // .option('config', {alias: 'c', describe: 'SDR config file'})
+  // .version()
+  // .help()
+  // .argv;
+
+  program
+    .option('-i, --ip <type>', 'SDRangel IP address', '127.0.0.1:8091')
+    .option('-c, --config <type>', 'SDR config file')
+    .parse(process.argv);
+
+  const opts = program.opts();
+
+  const rp = rpg(
+    http.request,
+    opts.ip,
+    ['', 'sdrangel'],
+    console
+  );
+
+
   // server
   const app = express();
   app.use(bodyParser.json());
@@ -101,9 +103,9 @@ const main = async () => {
     const fOffset = ((req.body || {}).SSBDemodSettings || {}).inputFrequencyOffset;
     if (fOffset !== undefined) {
       // console.log(fOffset);
-      const tx0 = await rp({uri: tx('channel', 0, 'settings'), json: true});
+      const tx0 = await rp({path: [...tx, 'channel', 0, 'settings']});
       tx0.SSBModSettings.inputFrequencyOffset = fOffset;
-      await rp({method: 'PATCH', uri: tx('channel', 0, 'settings'), json: true, body: tx0});
+      await rp({method: 'PATCH', path: [...tx, 'channel', 0, 'settings'], body: tx0});
     }
   });
   const server = http.createServer(app);
@@ -112,7 +114,7 @@ const main = async () => {
     const addr = 'http://' + ip.address() + ':' + server.address().port + '/';
     console.log(addr);
     await msleep(1000);
-    await initConfig();
+    await initConfig(rp, opts);
   });
 };
 
